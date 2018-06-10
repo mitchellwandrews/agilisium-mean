b7c153a9724f59d947e7bc2c22e5d3a61032112b
Mitchell Andrews <mitchellw.andrews@gmail.com>
2018-06-09 23:47:28 -0700

    heroku node_env mongo fix undo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect("mongodb://mandrews:K3I5pxfyvCx0mOPl@cluster0-shard-00-00-npikj.mongodb.net:27017,cluster0-shard-00-01-npikj.mongodb.net:27017,cluster0-shard-00-02-npikj.mongodb.net:27017/ProductMasterSalesData?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
   .then(connection => {
 
   })
